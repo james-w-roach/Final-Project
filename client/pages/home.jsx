b8fbd2d2e07c2f4fc91b1d9eba319c8459609505
James Roach <roachjweston@gmail.com>
2021-04-06 22:20:06 +0000

    home.jsx now renders header and createbutton

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from '../components/header';
+import CreateButton from '../components/createButton';
 
 export default function Home(props) {
   return (
     <>
+      <Header/>
+      <CreateButton/>
     </>
   );
 }
