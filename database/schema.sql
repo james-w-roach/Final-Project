2ad26ea3567cc696c4f4797018641a214122292a
James Roach <roachjweston@gmail.com>
2021-04-07 18:23:57 +0000

    added app.post method to index.js, added error middleware, created db table

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,10 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+create table "public"."itineraries" (
+  "tripId"      serial,
+  "tripName"    text           not null,
+  "createdAt"   timestamptz(6) not null default now(),
+  primary key ("tripId")
+)
--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
+const ClientError = require('./error-middleware');
+const pg = require('pg');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
 
@@ -9,4 +18,23 @@ app.use(staticMiddleware);
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
+});
+
+app.post('/planner/itineraries', (req, res, next) => {
+  const { tripName } = req.body;
+  if (!tripName) {
+    throw new ClientError(400, 'please enter an itinerary name');
+  }
+  const sql = `
+     insert into "itineraries" ("tripName")
+          values ($1)
+       returning "tripName"
+  `;
+  const params = [tripName];
+  db.query(sql, params)
+    .then(result => {
+      const [trip] = result.row;
+      res.status(201).json(trip);
+    })
+    .catch(err => next(err));
 });
