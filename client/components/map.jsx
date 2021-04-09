4ed3a809cb7a648c69725d3aa325a4503b0145cf
James Roach <roachjweston@gmail.com>
2021-04-09 19:06:57 +0000

    added form to render method of map.jsx, created handleAdd method in map.jsx

--- client/components/map.jsx
+++ client/components/map.jsx
@@ -13,9 +13,11 @@ class Mapbox extends React.Component {
     this.state = {
       lng: -70.9,
       lat: 42.35,
-      zoom: 9
+      zoom: 9,
+      location: ''
     };
     this.mapContainer = React.createRef();
+    this.handleAdd = this.handleAdd.bind(this);
   }
 
   componentDidMount() {
@@ -33,7 +35,6 @@ class Mapbox extends React.Component {
         lat: map.getCenter().lat.toFixed(4),
         zoom: map.getZoom().toFixed(2)
       });
-      console.log(this.state);
     });
 
     map.addControl(
@@ -41,14 +42,30 @@ class Mapbox extends React.Component {
         accessToken: mapboxgl.accessToken,
         mapboxgl: mapboxgl
       })
-        .on('result', result => console.log(result.result['place_name_en-US']))
+        .on('result', result => {
+          const locationName = result.result['place_name_en-US'];
+          this.setState({ location: locationName });
+        })
     );
+  }
+
+  handleAdd() {
+    event.preventDefault();
+    const newLocation = {
+      lng: this.state.lng,
+      lat: this.state.lat,
+      location: this.state.location
+    };
+    this.props.onSubmit(newLocation);
   }
 
   render() {
     return (
       <div>
         <div ref={this.mapContainer} className="map-container" />
+        <form onSubmit={this.handleAdd}>
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
