1e1d2090f3ef93750f03ad0ef4a6ed0bc22cd3fa
James Roach <roachjweston@gmail.com>
2021-04-12 23:59:44 +0000

    added font awesome link to html, added FA icons to locationsList

--- client/components/viewTrip.jsx
+++ client/components/viewTrip.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 export default class ViewTrip extends React.Component {
   render() {
     const { name, locations } = this.props.trip;
-    const locationsList = locations.map(location => <li key={location.split(',')[0]}>{location}</li>);
+    const locationsList = locations.map(location => {
+      return (
+        <li className="trip-list-item" key={location.split(',')[0]}>
+          {location}
+          0 <i className="fas fa-utensils"></i>
+          0 <i className="fas fa-map-marker-alt"></i>
+        </li>
+      );
+    });
     return (
       <>
         <h1>{name}</h1>
