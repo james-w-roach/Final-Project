cd5c4a2edb421e5b59bef644967c1e5a1ac36512
James Roach <roachjweston@gmail.com>
2021-04-13 01:56:29 +0000

    styled itinerary page to match wireframe

--- client/components/viewTrip.jsx
+++ client/components/viewTrip.jsx
@@ -7,15 +7,19 @@ export default class ViewTrip extends React.Component {
       return (
         <li className="trip-list-item" key={location.split(',')[0]}>
           {location}
-          0 <i className="fas fa-utensils"></i>
-          0 <i className="fas fa-map-marker-alt"></i>
+          <div className="icons">
+            0 <i className="fas fa-utensils"></i>
+            0 <i className="fas fa-map-marker-alt"></i>
+          </div>
         </li>
       );
     });
     return (
       <>
-        <h1>{name}</h1>
-        <ul>{locationsList}</ul>
+        <div className="main">
+          <h1 className="name">{name}</h1>
+          <ul>{locationsList}</ul>
+        </div>
       </>
     );
   }
