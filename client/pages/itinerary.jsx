0aa08a19733d26465951e2ac01bba2d46924767e
James Roach <roachjweston@gmail.com>
2021-04-13 21:41:52 +0000

    added toggleView method to itinerary

--- client/pages/itinerary.jsx
+++ client/pages/itinerary.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import ViewTrip from '../components/viewTrip';
+import ViewLocation from '../components/viewLocation';
 import Header from '../components/header';
 
 export default class Itinerary extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.toggleView = this.toggleView.bind(this);
+  }
+
+  toggleView(view) {
     const trip = this.props.trip;
+    if (view === 'location') {
+      return <ViewTrip trip={trip} toggleView={this.toggleView} />;
+    } else {
+      return <ViewLocation toggleView={this.toggleView} />;
+    }
+  }
+
+  render() {
     return (
       <>
         <Header />
-        <ViewTrip trip={trip} />
+        { this.toggleView() }
       </>
     );
   }
