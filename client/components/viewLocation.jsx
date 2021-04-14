d4348979517c8cb8248dc6974fc57c8e73cd7ed0
James Roach <roachjweston@gmail.com>
2021-04-14 20:00:37 +0000

    updated viewLocation render method to show location name, restaurants/POI buttons

--- client/components/viewLocation.jsx
+++ client/components/viewLocation.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 
 export default class ViewLocation extends React.Component {
   render() {
-    return <h1 onClick={() => this.props.toggleView()}>Hello World</h1>;
+    return (
+     <>
+        <div>
+          <i onClick={() => this.props.toggleView()} className="fas fa-arrow-left"></i>
+          <h1>{this.props.location.name}</h1>
+        </div>
+        <div>
+          <div className="trip-list-item">
+            Points of Interest
+            <i className="fas fa-arrow-right"></i>
+          </div>
+          <div className="trip-list-item">
+            Restaurants
+            <i className="fas fa-arrow-right"></i>
+          </div>
+        </div>
+      </>
+    );
   }
 }
