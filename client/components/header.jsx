1573fd3203ccb381738a993c7c02c54054810d4f
James Roach <roachjweston@gmail.com>
2022-10-12 23:11:32 +0000

    redesigned header anchors, extended header to width of screen

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -16,23 +16,19 @@ export default class Header extends React.Component {
     }
     return (
       <div className="header">
-        <div className="page-container">
-          <div className="header-content">
-            <a className="h1-anchor" href={''}><h1>Voyager</h1></a>
-            <div className="header-nav">
-              <a className="header-anchor" href={createHref}>Create</a>
-              <a className="header-anchor" href={listHref}>Itineraries</a>
-              <a className="header-anchor"
-                onClick={() => {
-                  if (this.props.loggedIn) {
-                    this.props.onSignOut();
-                  }
-                }}
-              href={loginHref}>{content}</a>
-            </div>
-            <i className="fas fa-bars drawer-button" id="drawer-button" onClick={this.props.showDrawer}></i>
-          </div>
+        <a className="h1-anchor" href={''}><h1>Voyager</h1></a>
+        <div className="header-nav">
+          <a className="header-anchor" href={createHref}>Create<div className='anchor-line'></div></a>
+          <a className="header-anchor" href={listHref}>Itineraries<div className='anchor-line'></div></a>
+          <a className="header-anchor"
+            onClick={() => {
+              if (this.props.loggedIn) {
+                this.props.onSignOut();
+              }
+            }}
+            href={loginHref}>{content}<div className='anchor-line'></div></a>
         </div>
+        <i className="fas fa-bars drawer-button" id="drawer-button" onClick={this.props.showDrawer}></i>
       </div>
     );
   }
