b8925b33558206ca8ed70f4ebea36f78d256426a
James Roach <roachjweston@gmail.com>
2022-12-17 03:57:54 +0000

    itinerary map switching in progress

--- client/components/itineraryMap.jsx
+++ client/components/itineraryMap.jsx
@@ -16,6 +16,73 @@ export default class ItineraryMap extends React.Component {
     this.mapContainer = React.createRef();
   }
 
+  componentDidUpdate() {
+    document.querySelector('.mapboxgl-canary').remove();
+    document.querySelector('.mapboxgl-canvas-container').remove();
+    document.querySelector('.mapboxgl-control-container').remove();
+
+    const { zoom } = this.state;
+
+    if (this.props.activeItinerary.locations.length === 1) {
+      const lng = this.props.activeItinerary
+        ? this.props.activeItinerary.locations[0].lng
+        : '';
+      const lat = this.props.activeItinerary
+        ? this.props.activeItinerary.locations[0].lat
+        : '';
+      const map = new mapboxgl.Map({
+        container: this.mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [lng, lat],
+        zoom: zoom
+      });
+      const marker = new mapboxgl.Marker()
+        .setLngLat([lng, lat])
+        .addTo(map);
+    } else {
+      const lngLats = this.props.activeItinerary.locations.map(location => {
+        const marker = new mapboxgl.Marker()
+          .setLngLat([location.lng, location.lat])
+          .addTo(map);
+        return { lng: location.lng, lat: location.lat };
+      });
+
+      let lngMin = lngLats[0].lng;
+      let lngMax = lngLats[0].lng;
+      let latMin = lngLats[0].lat;;
+      let latMax = lngLats[0].lat;;
+
+      for (let i = 1; i < lngLats.length; i++) {
+        if (lngLats[i].lng < lngMin) {
+          lngMin = lngLats[i].lng;
+        }
+        if (lngLats[i].lng > lngMax) {
+          lngMax = lngLats[i].lng;
+        }
+        if (lngLats[i].lat < latMin) {
+          latMin = lngLats[i].lat;
+        }
+        if (lngLats[i].lat > latMax) {
+          latMax = lngLats[i].lat;
+        }
+      }
+
+      const bounds = [[lngMin - 2, latMin - 2], [lngMax + 2, latMax + 2]];
+
+      const map = new mapboxgl.Map({
+        container: this.mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        bounds
+      });
+
+      const markers = this.props.activeItinerary.locations.map(location => {
+        const marker = new mapboxgl.Marker()
+          .setLngLat([location.lng, location.lat])
+          .addTo(map);
+      });
+    }
+  }
+
   componentDidMount() {
     const { zoom } = this.state;
 
@@ -77,14 +144,6 @@ export default class ItineraryMap extends React.Component {
           .addTo(map);
       });
     }
-
-    /* map.on('move', () => {
-      this.setState({
-        lng: map.getCenter().lng.toFixed(4),
-        lat: map.getCenter().lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
-      });
-    }); */
   }
 
   render() {
